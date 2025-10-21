dcdcd60848af97cd0d34c51c08c147683618d03a
mugisha thicien <mugishathicien04@gmail.com>
2025-10-21 13:57:50 +0200

    files added

--- merge.js
+++ merge.js
@@ -1,5 +1,13 @@
-const map1 = (
+const map1 = new Map([
     ["banana", 15],
     ["mango", 20],
     ["orange", 40]
-);
+    
+]);
+const map2 = new Map([
+    ["banana", 25],
+    ["mangoes", 20],
+    ["oranges", 40]
+]);
+const mergeMap = new Map([...map1, ...map2]);
+console.log(mergeMap);
