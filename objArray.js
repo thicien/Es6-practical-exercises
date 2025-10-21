0bd223f45bb245eebf779eef17d51bd234ca8d38
mugisha thicien <mugishathicien04@gmail.com>
2025-10-21 09:35:42 +0200

    students

---
+++ objArray.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' }
+];
+
+const studentMap = new Map(students.map(student => [student.id, student.name]));
+console.log(studentMap);
