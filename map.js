68147a214241ef0cb688b8fb18585e1d1cddbd00
mugisha thicien <mugishathicien04@gmail.com>
2025-10-21 09:05:33 +0200

    updates has made

--- map.js
+++ map.js
@@ -1,3 +1,18 @@
-const profile = {
-    
+const students = new Map();
+students.set("claire", 56);
+students.set("carine", 87);
+students.set("kevin", 95);
+console.log("initial output:", students);
+
+students.set("claire", 67);
+console.log("After some updates:", students);
+
+let highestGrade = Infinity;
+let topStudent = "";
+for (const [name, grade] of students) {
+    if (grade > highestGrade) {
+        highestGrade = grade;
+        topStudent = name;
+    }
 }
+console.log(`"The highest grade is:", ${highestGrade} by ${topStudent}`);
