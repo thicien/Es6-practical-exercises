2c523f6164a7d92ffdb277c305b1b97c917cc4e5
mugisha thicien <mugishathicien04@gmail.com>
2026-02-09 17:46:45 +0200

    Converting the array into object based results with the help of reduce method

--- returnObjectFromArray.js
+++ returnObjectFromArray.js
@@ -4,25 +4,24 @@ const users = [
   { id: 3, name: "Charlie", role: "admin", age: 35 },
   { id: 4, name: "David", role: "user", age: 28 },
   { id: 5, name: "Eve", role: "guest", age: 19 }
+  {id: 6, name: "Kevin", role: "guest", age: 21}
 ];
 const result = users.reduce((acc, user) => {
-  const { role, name, age } = user;
-
-  if (!acc[role]) {
-      acc[role] = {
-      totalUsers: 0,
-      names: [],
-      totalAge: 0,
-      averageAge: 0
-    };
-  }
-
-  acc[role].totalUsers += 1;
-  acc[role].names.push(name);
-  acc[role].totalAge += age;
-  acc[role].averageAge = acc[role].totalAge / acc[role].totalUsers;
-
-  return acc;
-}, {});
-
+    const { role, name, age } = user;
+    if (!acc[role]) {
+        acc[role] = {
+            totatlUsers: 0,
+            totatAge: 0,
+            names: [],
+            AverageAge: 0
+        }
+    }
+    acc[role].totatAge += age;
+    acc[role].totatlUsers += 1;
+    acc[role].names.push(name);
+    acc[role].AverageAge = acc[role].totatAge / acc[role].totatlUsers;
+    acc[role].totalNames = acc[role].names.length;
+    return acc;
+}, {})
 console.log(result);
+console.log(result['admin'].names);
